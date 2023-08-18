45c7fbf1c0608b845f0042a8d87e750321c6a729
ci7lus <7887955+ci7lus@users.noreply.github.com>
2023-08-18 16:44:18 +0900

    :bug: fix t2iSkipSensitiveWarning

--- app/routes/$path.tsx
+++ app/routes/$path.tsx
@@ -169,6 +169,7 @@ export async function loader({
       page.on("request", async (req) => {
         let url = req.url()
         if (url.startsWith("https://cdn.syndication.twimg.com/tweet-result")) {
+          const r = await axios.get(url)
           req.respond({
             status: 200,
             contentType: "application/json",
@@ -191,8 +192,6 @@ export async function loader({
     const params = {
       dnt: "false",
       embedId: "twitter-widget-0",
-      features:
-        "eyJ0ZndfdGltZWxpbmVfbGlzdCI6eyJidWNrZXQiOltdLCJ2ZXJzaW9uIjpudWxsfSwidGZ3X2ZvbGxvd2VyX2NvdW50X3N1bnNldCI6eyJidWNrZXQiOnRydWUsInZlcnNpb24iOm51bGx9LCJ0ZndfdHdlZXRfZWRpdF9iYWNrZW5kIjp7ImJ1Y2tldCI6Im9uIiwidmVyc2lvbiI6bnVsbH0sInRmd19yZWZzcmNfc2Vzc2lvbiI6eyJidWNrZXQiOiJvbiIsInZlcnNpb24iOm51bGx9LCJ0ZndfZm9zbnJfc29mdF9pbnRlcnZlbnRpb25zX2VuYWJsZWQiOnsiYnVja2V0Ijoib24iLCJ2ZXJzaW9uIjpudWxsfSwidGZ3X21peGVkX21lZGlhXzE1ODk3Ijp7ImJ1Y2tldCI6InRyZWF0bWVudCIsInZlcnNpb24iOm51bGx9LCJ0ZndfZXhwZXJpbWVudHNfY29va2llX2V4cGlyYXRpb24iOnsiYnVja2V0IjoxMjA5NjAwLCJ2ZXJzaW9uIjpudWxsfSwidGZ3X3Nob3dfYmlyZHdhdGNoX3Bpdm90c19lbmFibGVkIjp7ImJ1Y2tldCI6Im9uIiwidmVyc2lvbiI6bnVsbH0sInRmd19kdXBsaWNhdGVfc2NyaWJlc190b19zZXR0aW5ncyI6eyJidWNrZXQiOiJvbiIsInZlcnNpb24iOm51bGx9LCJ0ZndfdXNlX3Byb2ZpbGVfaW1hZ2Vfc2hhcGVfZW5hYmxlZCI6eyJidWNrZXQiOiJvbiIsInZlcnNpb24iOm51bGx9LCJ0ZndfdmlkZW9faGxzX2R5bmFtaWNfbWFuaWZlc3RzXzE1MDgyIjp7ImJ1Y2tldCI6InRydWVfYml0cmF0ZSIsInZlcnNpb24iOm51bGx9LCJ0ZndfbGVnYWN5X3RpbWVsaW5lX3N1bnNldCI6eyJidWNrZXQiOnRydWUsInZlcnNpb24iOm51bGx9LCJ0ZndfdHdlZXRfZWRpdF9mcm9udGVuZCI6eyJidWNrZXQiOiJvbiIsInZlcnNpb24iOm51bGx9fQ==",
       frame: "false",
       hideCard: hideCard.toString(),
       hideThread: hideThread.toString(),
@@ -200,7 +199,7 @@ export async function loader({
       lang,
       origin: "file:///Users/ci7lus/tweet2image.html",
       theme,
-      widgetsVersion: "aaf4084522e3a:1674595607486",
+      widgetsVersion: "a3525f077c700:1667415560940",
     }
     await Promise.allSettled([
       page.goto(
@@ -208,6 +207,7 @@ export async function loader({
           params
         )}`
       ),
+      page.waitForNavigation({ waitUntil: ["networkidle0"] }),
       page.waitForSelector("article", { timeout: 1000 }),
     ])
     const rect = await page.evaluate(() => {
